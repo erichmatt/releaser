4de9664e067c3c92a4ca066b2402e49f1b605170
erichmatt <erichmatt@gmail.com>
2023-03-23 12:09:28 -0400

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-#echo Boobies!!
-#password="your password"
-#username="dh_m958u5"
-#Ip="vps10240.dreamhostps.com"
-#sshpass -p "$password" scp scptest.txt $username@$Ip:scptest.txt>
+echo Boobies!!
+password="your password"
+username="dh_m958u5"
+Ip="vps10240.dreamhostps.com"
+sshpass -p "$password" scp scptest.txt $username@$Ip:scptest.txt>
 
-# connect via scp
-spawn scp "dh_m958u5@vps10240.dreamhostps.com:scptest.txt" scptest.txt
-#######################
-expect {
-  -re ".*es.*o.*" {
-    exp_send "yes\r"
-    exp_continue
-  }
-  -re ".*sword.*" {
-    exp_send "PASSWORD\r"
-  }
-}
-interact
+
